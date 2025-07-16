2ec1090c1373a8f654e9afa5d2c3a2980460fd99
smart <smart@smhrd>
2025-07-16 15:54:10 +0900

    첫번째 푸쉬 작업을 진행했습니다.

---
+++ github11/src/main/java/com/smhrd/web/Github11Application.java
@@ -0,0 +1,13 @@
+package com.smhrd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Github11Application.class, args);
+	}
+
+}
