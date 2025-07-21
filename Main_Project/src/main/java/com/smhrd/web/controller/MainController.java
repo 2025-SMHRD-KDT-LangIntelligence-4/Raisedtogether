14e2fe1b40811365bf9b7e788e0f4355aecd8f57
smart <smart@smhrd>
2025-07-21 13:59:45 +0900

    컨트롤러에 매퍼 불러옴

--- Main_Project/src/main/java/com/smhrd/web/controller/MainController.java
+++ Main_Project/src/main/java/com/smhrd/web/controller/MainController.java
@@ -1,5 +1,11 @@
 package com.smhrd.web.controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.smhrd.web.mapper.Mapper;
 
+@Controller
 public class MainController {
-
+	@Autowired
+	Mapper mapper;
 }
