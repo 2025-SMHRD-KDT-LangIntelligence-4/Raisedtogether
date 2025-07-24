ea4f2ae4adc583c1b6607802b5c5ed70d3f87b15
smart <smart@smhrd>
2025-07-24 17:49:29 +0900

    아이디, 닉네임 중복확인 로직 만듦 register.js

--- Main_Project/src/main/resources/static/js/register.js
+++ Main_Project/src/main/resources/static/js/register.js
@@ -1,3 +1,29 @@
-/**
- * 
- */
+function checkDuplicate(type){
+	let value;
+	
+	// type이 id인지 nickname인지에 따라 적절한 input값을 가져옴
+	if (type === "id") {
+		value = $("#inputId").val().trim();
+	} else if (type === "nickname") {
+		value = $("#inputNickname").val().trim();
+	}
+	
+	// 값이 비어있으면 값을 입력하라고 알려줌
+	if (!value) {
+		alert("값을 입력해주세요.");
+		return;
+	}
+	
+	$.ajax({
+		url : "/checkDuplicate",
+		type : "POST",
+		contentType : "application/json",
+		data : JSON.stringify({type : type, value : value}),
+		success : function(msg){
+			alert(msg);
+		},
+		error: function() {
+			alert("중복확인 중 오류 발생");
+		}
+	});
+}
